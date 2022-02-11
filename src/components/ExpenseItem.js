3aea9453fac3b02a7c59cf8945bbdda4808fb28e
volkangurbuz <volkangurbuz16@gmail.com>
2022-02-11 23:39:38 +0100

    added basic css style

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem(){
-    return <h2>Expense item!</h2>;
+//js code will tranform to jsx code on the browser
+import './ExpenseItem.css';
+
+
+function ExpenseItem() {
+    return (<div className="expense-item">
+        <div>
+            March 28th 2022
+        </div>
+        <div className="expense-item__description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">$294</div>
+        </div>
+    </div>);
 }
 
 
