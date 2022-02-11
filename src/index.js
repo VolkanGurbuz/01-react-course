43d55c738926a638c9fe99dea5c6dd1a980be635
volkangurbuz <volkangurbuz16@gmail.com>
2022-02-11 23:28:01 +0100

    building a first custom component

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+//will wanna render in this place, div will replace with App
+//app is component will render with id of root
+//component is a just js function
 ReactDOM.render(<App />, document.getElementById('root'));
