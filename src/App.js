3444da37f96fa30dfb36e6d1ee689fac9e2c4e7a
volkangurbuz <volkangurbuz16@gmail.com>
2022-02-12 19:58:01 +0100

    passing data via props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 
-
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-        <ExpenseItem></ExpenseItem>
-    </div>
-  );
+    const expenses = [{id: 'e', title: 'new desk', amount: 450, date: new Date(2021, 5, 12)}, {
+        id: 'e2',
+        title: 'new des2k',
+        amount: 410,
+        date: new Date(2021, 5, 3)
+    }];
+
+
+    return (
+        <div>
+            <h2>Let's get started!</h2>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+        </div>
+    );
 }
 
 export default App;
