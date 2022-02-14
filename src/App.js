d498e536281b0f0fcec260316d0482a662cd4d2c
volkangurbuz <volkangurbuz16@gmail.com>
2022-02-14 11:22:12 +0100

    rendered list to refresh the component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import Expenses from "./components/expenses/Expenses";
-import React from "react";
+import React, {useState} from "react";
 import NewExpense from "./components/newexpense/NewExpense";
+
+
+const DUMMY_EXPENSES = [{id: 'e', title: 'new desk', amount: 450, date: new Date(2021, 5, 12)}, {
+    id: 'e2',
+    title: 'new des2k',
+    amount: 410,
+    date: new Date(2021, 5, 3)
+}];
 
 //arrow function
 
 const App = () => {
 
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-    const expenses = [{id: 'e', title: 'new desk', amount: 450, date: new Date(2021, 5, 12)}, {
-        id: 'e2',
-        title: 'new des2k',
-        amount: 410,
-        date: new Date(2021, 5, 3)
-    }];
+
     //alternative
 
     // return React.createElement('div', {}, React.createElement('h2', {}, 'Lets get started'), React.createElement(Expenses, {items: expenses}))
 
-    const addExpenseHandler = expense => {
-        console.log("in app.js");
-        console.log(expense);
-    }
-
-
+    const addExpenseHandler = (expense) => {
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
+    };
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler}/>
             <Expenses items={expenses}/>
         </div>
-    )
-        ;
+    );
+
 }
+
 
 export default App;
